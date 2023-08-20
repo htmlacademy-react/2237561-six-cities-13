e4c0934514caa8e651647a7892af936f7019f38a
Tina <leskovskaya.ti@gmail.com>
2023-08-20 15:13:58 +0300

    правки логина, все заработало

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,16 @@
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import Logo from '../../components/logo/logo';
 import { useRef, FormEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/index';
 import { loginAction } from '../../store/api-actions';
 import { AppRoute } from '../../const';
+import Logo from '../../components/logo/logo';
 
 function LoginScreen(): JSX.Element {
   const loginRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
 
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
@@ -76,7 +75,6 @@ function LoginScreen(): JSX.Element {
               <button
                 className="login__submit form__submit button"
                 type="submit"
-                onClick={() => navigate(AppRoute.Main)}
               >
                 Sign in
               </button>
@@ -84,9 +82,9 @@ function LoginScreen(): JSX.Element {
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
-              </a>
+              <Link className="locations__item-link" to={AppRoute.Main}>
+                <span>Paris</span>
+              </Link>
             </div>
           </section>
         </div>
