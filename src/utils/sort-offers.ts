9ccf0314d041011d0ce6cfc834a6b34ad03f195d
Tina <leskovskaya.ti@gmail.com>
2023-08-29 20:22:37 +0300

    удалила неиспользуемые данные

--- src/utils/sort-offers.ts
+++ src/utils/sort-offers.ts
@@ -1,5 +1,4 @@
 import { TOffer } from '../types/offer';
-//import {TSorting} from '../types/sorting';
 
 const STARS_COUNT = 5;
 
@@ -16,12 +15,4 @@ function sortHighToLow (a: TOffer, b: TOffer){
   return b.price - a.price;
 }
 
-/*const sorting: Record <TSorting, (offers: TOffer[]) => TOffer[]> =
-{
-  Popular: (offers: TOffer[]) => offers.slice(),
-  HighToLow: (offers: TOffer[]) => offers.slice().sort(sortHighToLow),
-  LowToHigh: (offers: TOffer[]) => offers.slice().sort(sortLowToHigh),
-  TopRated: (offers: TOffer[]) => offers.slice().sort(sortByRating),
-};
-*/
 export {getRating, sortByRating, sortLowToHigh, sortHighToLow};
