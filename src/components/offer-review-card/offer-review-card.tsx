a7ef2a8ae4a2adf79ad126086d63a65f0765783c
Tina <leskovskaya.ti@gmail.com>
2023-08-29 20:49:30 +0300

    изменяет страницу not-found

--- src/components/offer-review-card/offer-review-card.tsx
+++ src/components/offer-review-card/offer-review-card.tsx
@@ -20,39 +20,42 @@ export default function OfferReviewCard({
       </h2>
 
       <ul className="reviews__list">
-        {reviews && sortReviewsByDate(reviews).slice(0,REVIEW_LIMIT).map((review) => (
-          <li className="reviews__item" key={review.id}>
-            <div className="reviews__user user">
-              <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                <img
-                  className="reviews__avatar user__avatar"
-                  src={review.user.avatarUrl}
-                  width="54"
-                  height="54"
-                  alt={review.user.name}
-                />
-              </div>
-              <span className="reviews__user-name">{review.user.name}</span>
-            </div>
-            <div className="reviews__info">
-              <div className="reviews__rating rating">
-                <div className="reviews__stars rating__stars">
-                  <span
-                    style={{
-                      width: `${Math.round(review.rating) * RATING_COEF}%`,
-                    }}
-                  >
-                  </span>
-                  <span className="visually-hidden">Rating</span>
+        {reviews &&
+          sortReviewsByDate(reviews)
+            .slice(0, REVIEW_LIMIT)
+            .map((review) => (
+              <li className="reviews__item" key={review.id}>
+                <div className="reviews__user user">
+                  <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                    <img
+                      className="reviews__avatar user__avatar"
+                      src={review.user.avatarUrl}
+                      width="54"
+                      height="54"
+                      alt={review.user.name}
+                    />
+                  </div>
+                  <span className="reviews__user-name">{review.user.name}</span>
                 </div>
-              </div>
-              <p className="reviews__text">{review.comment}</p>
-              <time className="reviews__time" dateTime={review.date}>
-                {formatDate(review.date)}
-              </time>
-            </div>
-          </li>
-        ))}
+                <div className="reviews__info">
+                  <div className="reviews__rating rating">
+                    <div className="reviews__stars rating__stars">
+                      <span
+                        style={{
+                          width: `${Math.round(review.rating) * RATING_COEF}%`,
+                        }}
+                      >
+                      </span>
+                      <span className="visually-hidden">Rating</span>
+                    </div>
+                  </div>
+                  <p className="reviews__text">{review.comment}</p>
+                  <time className="reviews__time" dateTime={review.date}>
+                    {formatDate(review.date)}
+                  </time>
+                </div>
+              </li>
+            ))}
       </ul>
     </section>
   );
--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -5,33 +5,17 @@ import styles from './not-found.module.css';
 
 function NotFoundScreen(): JSX.Element {
   return (
-    <div className="page not-found">
+    <>
       <Helmet>
         <title>6 cities not found</title>
       </Helmet>
-      <main className="page main--not-found">
-        <div className="page not-found-container container">
-          <section className="page not-found">
-            <div className="page__not-found-wrapper">
-              <img
-                className="not-found"
-                src="https://i.ibb.co/qsQr6mL/pngwing-com.png"
-                alt="6 cities not found"
-                width="100%"
-                height="100%"
-                style={{ textAlign: 'center', marginTop: '15%' }}
-              />
-              <p className={styles.text}>
-                Вернуться на{' '}
-                <Link to={AppRoute.Main} className={styles.link}>
-                  главную
-                </Link>
-              </p>
-            </div>
-          </section>
+      <div className={styles.wrapper}>
+        <h1>404 Not Found</h1>
+        <div className={styles.icon404}>
+          <Link to={AppRoute.Main}>Houston, we have a problem. Try again!</Link>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
 export default NotFoundScreen;
